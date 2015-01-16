b1b3ada23b41affb4d7792d19602896873477946
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-16 17:21:55 +0000

    Test passes. Slight change of phrasing of the test to make it clearer.

--- spec/jiit_spec.rb
+++ spec/jiit_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Jiit do 
 
-  context 'check can add jiit to database' do 
+  context 'check the database is working' do 
 
-    it 'should be created and then retrieved from the db' do 
+    it 'should be create a jiit and then retrieve it from the database' do 
 
       expect(Jiit.count).to eq(0)
       Jiit.create(text: 'Hello World!')
       expect(Jiit.count).to eq(1)
-      
+
     end
   end
 end
