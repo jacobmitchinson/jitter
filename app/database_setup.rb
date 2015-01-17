05c0eb8222d6ab91928a06fede987b706af1b7c9
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-17 16:04:19 +0000

    Updated database to create user model in the database.

--- app/database_setup.rb
+++ app/database_setup.rb
@@ -6,6 +6,7 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/jitter_#{env}")
 
 require './app/models/jiit'
+require './app/models/user'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
