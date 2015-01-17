ac2bbac89def245e7a91af3c8d9d641da07923c2
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-17 16:04:58 +0000

    Updated server to include user model and create new route/controller to /users/new.

--- app/server.rb
+++ app/server.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require_relative './models/jiit'
+require_relative './models/user'
 require_relative 'database_setup'
 
 get '/' do 
   @jiits = Jiit.all
   erb :index
+end
+
+get '/users/new' do 
+  erb :'/users/new'
 end
