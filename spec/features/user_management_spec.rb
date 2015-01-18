7c65fbb3c19225c41ae4885e8f1fdb2e932421cf
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-18 22:09:58 +0000

    Adjusted test and made it pass.

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
-feature 'In order to use jitter' do 
+feature 'In order to use jitter as a maker' do 
 
   scenario "I want to sign up to the service" do 
     expect{ sign_up }.to change(User, :count).by(1)
     expect(page).to have_content("Welcome to Jitter, Jake!")
     expect(User.first.email).to eq("jake@test.com")
     expect(User.first.name).to eq("Jake")
+  end
+
+  scenario "I need to know when I sign up with incorrect details" do 
+    expect{ sign_up('aa@.com', 'wrong', 'right', 'jake') }.to change(User, :count).by(0)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Sorry, your passwords don\'t match.')
   end
 
   def sign_up(email = "jake@test.com",
