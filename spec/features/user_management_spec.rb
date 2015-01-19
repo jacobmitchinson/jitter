ebfc8a3e051e47b5becce47cf81cc55d04ade8dd
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-19 09:16:23 +0000

    Added test for sign in .

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -21,6 +21,8 @@ feature 'In order to use jitter as a maker' do
     expect(page).to have_content("This email is already taken")
   end
 
+
+
   def sign_up(email = "jake@test.com",
               password = "blue",
               password_confirmation = "blue",
@@ -32,6 +34,32 @@ feature 'In order to use jitter as a maker' do
     fill_in :password, with: password
     fill_in :password_confirmation, with: password_confirmation
     click_button "Sign up"
+  end
+
+end
+
+feature 'In order to use chitter as a Maker I want to login' do
+
+
+  before(:each) do 
+    User.create(name: "jake",
+                email: "jake@test.com",
+                password: "jake",
+                password_confirmation: "jake")
+  end
+
+  scenario 'with correct credentials' do 
+    visit '/'
+    expect(page).not_to have_content('Welcome to Jitter, Jake!')
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content("Welcome to Jitter, Jake!")
+  end
+
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in 'email', :with => email
+    fill_in 'password', :with => password
+    click_button 'Sign in'
   end
 
 
